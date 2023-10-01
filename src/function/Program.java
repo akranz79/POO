87adae0ad85a370ed9ec14b6e512ab7cf974ca47
Alexandre Kranz <ahkranz79@gmail.com>
2023-10-01 20:28:41 -0300

    primeiro exercicio

---
+++ src/function/Program.java
@@ -0,0 +1,30 @@
+package function;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+	
+	
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		// TODO Auto-generated method stub
+		System.out.println("digite dois valores inteiros:");
+		int variavel_a = sc.nextInt();
+		sc.nextLine();
+		int variavel_b = sc.nextInt();
+		
+		int soma = variavel_a + variavel_b;
+		
+		System.out.println("SOMA: "+ soma);
+		
+		sc.close();
+		
+
+	}
+
+}
